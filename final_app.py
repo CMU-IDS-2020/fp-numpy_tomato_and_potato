2c2cfcab1d6dc1de4eba200866c19104811f9adf
greyring <greyring@163.com>
2020-12-09 20:56:20 -0500

    fix bug

--- final_app.py
+++ final_app.py
@@ -307,24 +307,40 @@ st.write("By default, we provide the top 6 genres whose total movie number is th
         
 genre_data = load_genre_data()
 imdb_rating = load_movie_imdb()
-genre_rating = transform_id_to_rating(genre_data.keys(), genre_data)
 first_level_genres = filter_genre_by_level(genre_data, _genre_level)
 
 selected_genres = st.multiselect('What genre are you interested in?', list(genre_data.keys()), default=first_level_genres)
-hist_fig = go.Figure()
-for genre in selected_genres:
-    hist_fig.add_trace(go.Histogram(x = genre_rating[genre],
-                       opacity=0.75, name = genre,
-                       xbins = dict(start = 0.0, end = 10.0, size = 0.2)))
-hist_fig.update_layout(barmode='stack', xaxis_title_text='Rating', yaxis_title_text='Number')
+
+@st.cache
+def get_genre_rating(genre_data):
+    return transform_id_to_rating(genre_data.keys(), genre_data)
+
+@st.cache
+def draw_hist_fig(selected_genres, genre_rating):
+    hist_fig = go.Figure()
+    for genre in selected_genres:
+        hist_fig.add_trace(go.Histogram(x = genre_rating[genre],
+                        opacity=0.75, name = genre,
+                        xbins = dict(start = 0.0, end = 10.0, size = 0.2)))
+    hist_fig.update_layout(barmode='stack', xaxis_title_text='Rating', yaxis_title_text='Number')
+    return hist_fig
+
+genre_rating = get_genre_rating(genre_data)
+hist_fig = draw_hist_fig(selected_genres, genre_rating)
 st.plotly_chart(hist_fig, use_container_width=True)
+
 st.write("As you can see from the histogram, most of the genres are rated between 6 and 8. However, from this histogram, we can't tell which genres tend to get a higher score. To explore the genre's factors deeper, we plot violin figures to visualize each genre's rating distribution.")
 
-violin_fig = go.Figure()
-for genre in selected_genres:
-    violin_fig.add_trace(go.Violin(y=genre_rating[genre], box_visible = True, 
-                    meanline_visible=True, name=genre))
+@st.cache
+def draw_violin_fig(selected_genres, genre_rating):
+    violin_fig = go.Figure()
+    for genre in selected_genres:
+        violin_fig.add_trace(go.Violin(y=genre_rating[genre], box_visible = True, 
+                        meanline_visible=True, name=genre))
+    return violin_fig
+violin_fig = draw_violin_fig(selected_genres, genre_rating)
 st.plotly_chart(violin_fig)
+
 st.write("According to the violin plots, drama is the highest-rated genre since its distribution is higher than the distribution of other genres. The median and max score for drama are also the highest.  Another interesting fact is that the Horror movie's rating is the lowest. The Horror movie's median score is 5.4, far less than other genres, and the overall distribution is also lower.")
 
 st.write("All in all, it's obvious that the genre factor truly has an impact on the movie rating, and drama is the highest-rated, while horror movies are the lowest rated.")
@@ -332,25 +348,42 @@ st.write("All in all, it's obvious that the genre factor truly has an impact on 
 
 st.markdown("# Beyond English")
 st.write("Similar to Genre, we also allow users to select the movie language by themselves, and we provide English, French, Italian, German, and Japanese languages by default.")
+
+@st.cache
+def get_lang_rating(language_data):
+    return transform_id_to_rating(language_data.keys(), language_data)
+
 language_data = load_language_data()
-language_rating = transform_id_to_rating(language_data.keys(), language_data)
+language_rating = get_lang_rating(language_data)
 first_level_languages = filter_genre_by_level(language_data, _language_level)
 selected_languages = st.multiselect('What language movies are you interested in?', 
                                     list(language_data.keys()), default=first_level_languages)
-language_hist_fig = go.Figure()
-for language in selected_languages:
-    language_hist_fig.add_trace(go.Histogram(x = language_rating[language],
-                                opacity = 0.75, name = language,
-                                xbins = dict(start = 0.0, end = 10.0, size = 0.2)))
-language_hist_fig.update_layout(barmode = 'stack', xaxis_title_text='Rating', yaxis_title_text='Number')
+
+
+@st.cache()
+def draw_lang_hist(selected_languages, language_rating):
+    language_hist_fig = go.Figure()
+    for language in selected_languages:
+        language_hist_fig.add_trace(go.Histogram(x = language_rating[language],
+                                    opacity = 0.75, name = language,
+                                    xbins = dict(start = 0.0, end = 10.0, size = 0.2)))
+    language_hist_fig.update_layout(barmode = 'stack', xaxis_title_text='Rating', yaxis_title_text='Number')
+    return language_hist_fig
+
+language_hist_fig = draw_lang_hist(selected_languages, language_rating)
 st.plotly_chart(language_hist_fig, use_container_width=True)
 
 st.write("In the diagram, we can see that the English language movie has the largest number. However, English movies' distribution is slightly lower than others (left in the Histogram figure). Also, we find that most of the movies are rated between 6.0 and 8.0. This is consistent with the previous genre's result. If you remove the English movies and keep the rest default movies, you can find an interesting phenomenon: the Japanese movies seem to have higher ratings than other movies. To reduce the movie number's factor and pay more attention to the distribution, we also plot the violin figure.")
 
-language_violin_fig = go.Figure()
-for language in selected_languages:
-    language_violin_fig.add_trace(go.Violin(y=language_rating[language],  box_visible = True,
-                            meanline_visible=True, name=language))
+@st.cache
+def draw_lang_violin(selected_languages, language_rating):
+    language_violin_fig = go.Figure()
+    for language in selected_languages:
+        language_violin_fig.add_trace(go.Violin(y=language_rating[language],  box_visible = True,
+                                meanline_visible=True, name=language))
+    return language_violin_fig
+
+language_violin_fig = draw_lang_violin(selected_languages, language_rating)
 st.plotly_chart(language_violin_fig)
 
 st.write("Firstly, the violin plot validates our guessing that Japanese movies usually are higher rated. Both the entire distribution and mathematical statistics (median, mean) imply it. Secondly, although the number of English movies is far higher than other language movies, the overall rating is lower than others. The reason might be that this dataset is collected in the USA; hence the foreign language movies are more likely to be high-rated, the low-rated language movies may not be imported to the U.S. Thirdly, we can conclude that Italian movies also don't perform well, the overall distribution is lower, and there are more low-rating Italian movies.")
@@ -359,73 +392,100 @@ st.write("To conclude, the original language of movies truly influences the rati
 
 st.markdown("# Bigger Budgets?")
 st.write("Both language and genre are discrete variables, and now we may focus more on continuous variables. Budget is an interesting variable. Naturally, we may think that a higher budget should obtain higher revenue while it might not increase the rating score since it might sacrifice some cultural value to appeal to more people.")
-budget_data = load_budget_data()
-budget_rating = []
-for budget, movie_list in budget_data.items():
-    for movie_id in movie_list:
-        budget_rating.append((budget, imdb_rating[movie_id]))
-budget_df = pd.DataFrame(budget_rating, columns=["budget", "rating"])
 
-budget_figure = px.scatter(budget_df, x="budget", y="rating")
-budget_figure.update_layout(xaxis_title_text='Budget', yaxis_title_text='Rating')
+budget_data = load_budget_data()
+
+@st.cache
+def draw_budget_figure(budget_data, imdb_rating):
+    budget_rating = []
+    for budget, movie_list in budget_data.items():
+        for movie_id in movie_list:
+            budget_rating.append((budget, imdb_rating[movie_id]))
+    budget_df = pd.DataFrame(budget_rating, columns=["budget", "rating"])
+    budget_figure = px.scatter(budget_df, x="budget", y="rating")
+    budget_figure.update_layout(xaxis_title_text='Budget', yaxis_title_text='Rating')
+    return budget_figure
+
+budget_figure = draw_budget_figure(budget_data, imdb_rating)
 st.plotly_chart(budget_figure)
+
 st.write("We first use a scatter plot to visualize the overall distribution. The X-axis is budget, and Y-axis is rating. The figure implies that there exists some relationship between budget and rating. For example, we find that low budget movies distribute from 0.0 to 10.0, which means there are both good movies and some terrible movies with low budget. With the budget increasing, we find that the number of bad movies (with low-rating score) decreases a lot. This finding implies that high budget movies will not be low-rated, at least. To validate my guess, I divide the budget into multiple groups 0~50M dollars, 50M~100M dollars, 100M~200M dollars, >200M dollars. Then, we can see the distribution in the different budget groups.")
 
-budget_bucket_rating = {"<50M":[], "50M~100M":[], "100M~200M":[], ">200M":[]}
-for budget, movie_list in budget_data.items():
-    budget = float(budget)
-    if budget < 50000000:
-        budget_key = "<50M"
-    elif budget < 100000000:
-        budget_key = "50M~100M"
-    elif budget < 200000000:
-        budget_key = "100M~200M"
-    else:
-        budget_key = ">200M"
-    for movie_id in movie_list:
-        budget_bucket_rating[budget_key].append(imdb_rating[movie_id])
+@st.cache
+def draw_budget_bucket(budget_data, imdb_rating):
+    budget_bucket_rating = {"<50M":[], "50M~100M":[], "100M~200M":[], ">200M":[]}
+    for budget, movie_list in budget_data.items():
+        budget = float(budget)
+        if budget < 50000000:
+            budget_key = "<50M"
+        elif budget < 100000000:
+            budget_key = "50M~100M"
+        elif budget < 200000000:
+            budget_key = "100M~200M"
+        else:
+            budget_key = ">200M"
+        for movie_id in movie_list:
+            budget_bucket_rating[budget_key].append(imdb_rating[movie_id])
 
-budget_bucket_figure = go.Figure()
-for budget_key in budget_bucket_rating:
-    budget_bucket_figure.add_trace(go.Box(y=budget_bucket_rating[budget_key], name=budget_key))
+    budget_bucket_figure = go.Figure()
+    for budget_key in budget_bucket_rating:
+        budget_bucket_figure.add_trace(go.Box(y=budget_bucket_rating[budget_key], name=budget_key))
+    return budget_bucket_figure
+
+budget_bucket_figure = draw_budget_bucket(budget_data, imdb_rating)
 st.plotly_chart(budget_bucket_figure)
+
 st.write("We use boxplots to visualize the distributions in different budget groups. Based on the figure, it's true that high budget movies' rating distribution is overall higher than the distributions of lower-budget movies. This finding implies that if you pay more, even if you might not get the highest rating, you will not obtain a too bad result. Also, we find that the highest rated movies lie in the 100M~200M group this is consistent with our common sense that medium-budget movies can be highly rated.")
 
 st.write("Hence, we think that the budget will also influence the movie rating.")
 
 st.markdown("# Long Movies? Short Movies?")
 st.write("Runtime is similar to Budget factor, to make an overview correlation between runtime and rating, we use a scatter plot to visualize and explore it.")
+
 runtime_data = load_runtime_data()
-runtime_rating = []
-for runtime, movie_list in runtime_data.items():
-    for movie_id in movie_list:
-        runtime_rating.append((runtime, imdb_rating[movie_id]))
-runtime_df = pd.DataFrame(runtime_rating, columns=["runtime", "rating"])
-runtime_figure = px.scatter(runtime_df, x="runtime", y="rating")
-runtime_figure.update_layout(xaxis_title_text='Runtime', yaxis_title_text='Rating')
+
+@st.cache
+def draw_runtime(runtime_data, imdb_rating):
+    runtime_rating = []
+    for runtime, movie_list in runtime_data.items():
+        for movie_id in movie_list:
+            runtime_rating.append((runtime, imdb_rating[movie_id]))
+    runtime_df = pd.DataFrame(runtime_rating, columns=["runtime", "rating"])
+    runtime_figure = px.scatter(runtime_df, x="runtime", y="rating")
+    runtime_figure.update_layout(xaxis_title_text='Runtime', yaxis_title_text='Rating')
+    return runtime_figure
+
+runtime_figure = draw_runtime(runtime_data, imdb_rating)
 st.plotly_chart(runtime_figure)
+
 st.write("We can conclude that the movies with runtime > 400 are usually highly rated from the runtime scatter plot, which might because these movies are documentaries, and documentaries are often rated high. Also, some short movies with runtime < 50 are also highly rated. These movies might be the micro-movies, which are also invariably highly rated. Overall, we conclude that there exists a trend that the movies with higher runtime might get higher scores. To validate our guessing, we plot some boxplot figures.")
 
-runtime_bucket_rating = {"<60 min":[], "60~90 min":[], "90~150 min":[],"150~240 min":[], ">240 min":[]}
-for runtime, movie_list in runtime_data.items():
-    runtime = float(runtime)
-    if runtime < 60:
-        runtime_key = "<60 min"
-    elif runtime < 90:
-        runtime_key = "60~90 min"
-    elif runtime < 150:
-        runtime_key = "90~150 min"
-    elif runtime < 240:
-        runtime_key = "150~240 min"
-    else:
-        runtime_key = ">240 min"
-    for movie_id in movie_list:
-        runtime_bucket_rating[runtime_key].append(imdb_rating[movie_id])
+@st.cache
+def draw_runtime_bucket(runtime_data, imdb_rating):
+    runtime_bucket_rating = {"<60 min":[], "60~90 min":[], "90~150 min":[],"150~240 min":[], ">240 min":[]}
+    for runtime, movie_list in runtime_data.items():
+        runtime = float(runtime)
+        if runtime < 60:
+            runtime_key = "<60 min"
+        elif runtime < 90:
+            runtime_key = "60~90 min"
+        elif runtime < 150:
+            runtime_key = "90~150 min"
+        elif runtime < 240:
+            runtime_key = "150~240 min"
+        else:
+            runtime_key = ">240 min"
+        for movie_id in movie_list:
+            runtime_bucket_rating[runtime_key].append(imdb_rating[movie_id])
 
-runtime_bucket_figure = go.Figure()
-for runtime_key in runtime_bucket_rating:
-    runtime_bucket_figure.add_trace(go.Box(y=runtime_bucket_rating[runtime_key], name=runtime_key))
+    runtime_bucket_figure = go.Figure()
+    for runtime_key in runtime_bucket_rating:
+        runtime_bucket_figure.add_trace(go.Box(y=runtime_bucket_rating[runtime_key], name=runtime_key))
+    return runtime_bucket_figure
+
+runtime_bucket_figure = draw_runtime_bucket(runtime_data, imdb_rating)
 st.plotly_chart(runtime_bucket_figure)
+
 st.write("The boxplot figure validates our guess. The movies whose runtime is between 60 and 150 minutes perform the worst, and the micro-movies (with runtime < 60 mins) perform better. From the second group (60~90) to the fifth group (>240 min), the distribution becomes gradually higher.")
 st.write("As a consequence, we can conclude that the runtime also makes some difference in the final rating. Longer movies tend to get higher rating scores, except for too-short movies, which also get high scores.")
 
